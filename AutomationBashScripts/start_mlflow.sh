a991b62c5be1cb49e213421e74a49c963d90c02c
santhosh <skumarr53@gmail.com>
2024-10-21 09:14:11 +0530

    mlflow

--- AutomationBashScripts/start_mlflow.sh
+++ AutomationBashScripts/start_mlflow.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Set the MLflow tracking URI
+# export MLFLOW_TRACKING_URI=sqlite:///mlflow.db  # Change this to your desired URI
+export MLFLOW_TRACKING_URI=./mlruns
+
+
 # Start the MLflow server
 mlflow server \
     --backend-store-uri sqlite:///mlflow.db \
-    --default-artifact-root ./mlruns \
+    --default-artifact-root $MLFLOW_TRACKING_URI \
     --host 0.0.0.0 \
     --port 5000
